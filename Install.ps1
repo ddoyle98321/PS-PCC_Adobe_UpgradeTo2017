e30b26db91a3889009aacd065b7f44fbe6011123
ddoyle98321 <ddoyle98321@gmail.com>
2017-01-31 09:16:24 -0800

    Version .01

---
+++ Install.ps1
@@ -0,0 +1,11 @@
+﻿$CN = "W7EW00001PCC02"
+$APV = Get-WmiObject -ComputerName $CN -Namespace "root\cimv2\sms" -Query "SELECT * FROM SMS_InstalledSoftware" | Where-Object ARPDisplayName -Like "Adobe Photoshop*" | Select-Object -ExpandProperty ProductVersion
+$APV = $APV.ToString().Substring(0,2)
+If ($APV -eq "18")
+    { 
+    Write-host "$APV It worked!!!"
+    }
+    Else
+    {
+    write-host "$APV Nope. Chuck Testa"
+    }
